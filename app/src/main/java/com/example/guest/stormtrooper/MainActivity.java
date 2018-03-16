80ec9308b9f6880a1f9eea33607e13ec5861eaf2
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-03-16 09:52:43 -0700

    add Weather Activity and activity_weather.xml

--- app/src/main/java/com/example/guest/stormtrooper/MainActivity.java
+++ app/src/main/java/com/example/guest/stormtrooper/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.guest.stormtrooper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findWeatherButton) Button mFindWeatherButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindWeatherButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick (View v) {
+        if(v == mFindWeatherButton){
+            Intent intent = new Intent(MainActivity.this,WeatherActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/guest/stormtrooper/WeatherActivity.java
@@ -0,0 +1,8 @@
+package com.example.guest.stormtrooper;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class WeatherActivity extends AppCompatActivity implements View.OnClickListener {
+}
