62efa6c1a116d84ce9ed49c9454bb9b7bf0b7f6e
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-03-16 10:09:41 -0700

    add weather activity and add it to manifest

--- app/src/main/java/com/example/guest/stormtrooper/WeatherActivity.java
+++ app/src/main/java/com/example/guest/stormtrooper/WeatherActivity.java
@@ -1,8 +1,32 @@
 package com.example.guest.stormtrooper;
 
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class WeatherActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WeatherActivity extends AppCompatActivity {
+    @BindView(R.id.returnWeatherButton) Button mReturnWeatherButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ButterKnife.bind(this);
+
+//        mReturnWeatherButton.setOnClickListener(this);
+
+    }
+
+//    @Override
+//    public void onClick(View v){
+//
+//    }
 }
