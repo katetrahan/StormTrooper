805512fc693bf1273690ab50aef8552730432c49
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-04-02 15:51:08 -0700

    add value event listener

--- app/src/main/java/com/example/guest/stormtrooper/ui/PlantNotesActivity.java
+++ app/src/main/java/com/example/guest/stormtrooper/ui/PlantNotesActivity.java
@@ -2,14 +2,18 @@ package com.example.guest.stormtrooper.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.guest.stormtrooper.Constants;
 import com.example.guest.stormtrooper.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,6 +21,7 @@ import butterknife.ButterKnife;
 public class PlantNotesActivity extends AppCompatActivity implements View.OnClickListener {
 
     private DatabaseReference mNotesReference;
+    private ValueEventListener mNoteReferenceListener;
 
     @BindView(R.id.saveNoteButton) Button mSaveNoteButton;
     @BindView(R.id.editNoteText) EditText mEditNoteText;
@@ -28,6 +33,23 @@ public class PlantNotesActivity extends AppCompatActivity implements View.OnClic
                 .getInstance()
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_NOTES);
+
+        mNoteReferenceListener = mNotesReference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot noteSnapshot : dataSnapshot.getChildren()) {
+                    String note = noteSnapshot.getValue().toString();
+                    Log.d("note updated", "note updated " + note);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_notes);
@@ -51,5 +73,12 @@ public class PlantNotesActivity extends AppCompatActivity implements View.OnClic
     public void saveNoteToFirebase(String note) {
         mNotesReference.push().setValue(note);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mNotesReference.removeEventListener(mNoteReferenceListener);
+    }
+
 
 }
