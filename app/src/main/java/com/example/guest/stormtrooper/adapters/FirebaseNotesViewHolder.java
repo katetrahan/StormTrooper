7264fcf1f5d19774b4559645128edcb79995a98e
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-04-06 16:16:10 -0700

    add XML animation

--- app/src/main/java/com/example/guest/stormtrooper/adapters/FirebaseNotesViewHolder.java
+++ app/src/main/java/com/example/guest/stormtrooper/adapters/FirebaseNotesViewHolder.java
@@ -1,18 +1,23 @@
 package com.example.guest.stormtrooper.adapters;
 
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.guest.stormtrooper.Constants;
 import com.example.guest.stormtrooper.R;
 import com.example.guest.stormtrooper.models.Note;
+import com.example.guest.stormtrooper.util.ItemTouchHelperViewHolder;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class FirebaseNotesViewHolder extends RecyclerView.ViewHolder {
+public class FirebaseNotesViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
 
     View mView;
     Context mContext;
@@ -35,6 +40,22 @@ public class FirebaseNotesViewHolder extends RecyclerView.ViewHolder {
 //        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_NOTES);
     }
 
+
+    @Override
+    public void onItemSelected() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_on);
+        set.setTarget(itemView);
+        set.start();
+    }
+
+    @Override
+    public void onItemClear() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
+    }
 
 
 
---
+++ app/src/main/java/com/example/guest/stormtrooper/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.guest.stormtrooper.util;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+
+public interface ItemTouchHelperViewHolder  {
+    void onItemSelected();
+    void onItemClear();
+
+
+
+}
